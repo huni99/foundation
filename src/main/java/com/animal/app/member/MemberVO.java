93085c25781f6e19a505c491d34e3d523350292d
eunbin <kozilee00@gmail.com>
2025-08-20 18:29:51 +0900

    Member 패키지 수정

--- src/main/java/com/animal/app/member/MemberVO.java
+++ src/main/java/com/animal/app/member/MemberVO.java
@@ -5,6 +5,7 @@ import com.animal.app.member.validation.UpdateGroup;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
