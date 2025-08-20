10828926d00be2d098805aee74d5bd7e5195348e
eunbin <kozilee00@gmail.com>
2025-08-20 12:01:31 +0900

    commons 패키지

--- src/main/java/com/animal/app/commons/FileVO.java
+++ src/main/java/com/animal/app/commons/FileVO.java
@@ -5,7 +5,7 @@ import com.animal.app.member.MemberVO;
 import lombok.Data;
 
 @Data
-public class FileVO extends MemberVO{
+public class FileVO extends MemberVO {
 	
 	private String oriName;
 	private String saveName;
